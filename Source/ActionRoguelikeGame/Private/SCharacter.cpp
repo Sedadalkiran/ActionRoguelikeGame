f20d65e7a7467802f3be9e145715030412d6a23c
Seda <sedadalkiran@gmail.com>
2022-09-06 21:31:56 +0300

    Character improvements

--- Source/ActionRoguelikeGame/Private/SCharacter.cpp
+++ Source/ActionRoguelikeGame/Private/SCharacter.cpp
@@ -3,6 +3,7 @@
 
 #include "SCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/Pawn.h"
 
@@ -17,10 +18,13 @@ ASCharacter::ASCharacter()
 
 	
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->bUsePawnControlRotation=true;
 	SpringArmComp->SetupAttachment(RootComponent);
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+	bUseControllerRotationYaw=false;
 }
 
 // Called when the game starts or when spawned
@@ -39,7 +43,20 @@ void ASCharacter::Tick(float DeltaTime)
 
 void ASCharacter::MoveForward(float value)
 {
-	AddMovementInput(GetActorForwardVector(),value);
+	FRotator ControlRot=GetControlRotation();
+	ControlRot.Pitch=0.0f;
+	ControlRot.Roll=0.0f;
+	AddMovementInput(ControlRot.Vector(),value);
+}
+
+void ASCharacter::MoveRight(float value)
+{
+	FRotator ControlRot=GetControlRotation();
+	ControlRot.Pitch=0.0f;
+	ControlRot.Roll=0.0f;
+
+	FVector RightVector=FRotationMatrix(ControlRot).GetScaledAxis(EAxis::Y);
+	AddMovementInput(RightVector,value);
 }
 
 // Called to bind functionality to input
@@ -48,6 +65,9 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+	
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
